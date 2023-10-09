25c59313064ea851191717bd9e9e1977626d8199
pix106 <sbordenave@gmail.com>
2023-10-09 17:04:34 +0200

    lavender: use libMegviiFacepp-0.5.2 stub

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     camera.sdm660 \
+    libMegviiFacepp-0.5.2 \
     Aperture \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
