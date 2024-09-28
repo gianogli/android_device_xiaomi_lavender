05fadc7ede1c9945a885d3b9ec76f9496c2dacd2
pix106 <sbordenave@gmail.com>
2024-09-28 11:32:06 +0200

    lavender: build com.fingerprints.extension@2.0.so from source

--- device.mk
+++ device.mk
@@ -148,6 +148,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprints
+PRODUCT_PACKAGES += \
+    com.fingerprints.extension@2.0.vendor
+
 # FM
 BOARD_HAVE_QCOM_FM := true
 
